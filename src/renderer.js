3d5d1782fcde1dead98c31c4a10226d12fa81b87
steamojikitsilano <109872892+steamojikitsilano@users.noreply.github.com>
2024-10-01 11:02:19 -0700

    a complete and functional browser using electron.js

---
+++ src/renderer.js
@@ -0,0 +1,42 @@
+import './index.css';
+
+
+
+
+// buttons
+
+const backButton = document.getElementById("back-button");
+const forwardButton = document.getElementById("forward-button");
+const reloadButton = document.getElementById("reload-button");
+const searchButton = document.getElementById("search-button");
+const newWindowButton = document.getElementById("new-window-button");
+const goButton = document.getElementById("go");
+
+
+// url field
+
+const urlInputField = document.getElementById("url-input");
+
+// webview
+
+const webview = document.getElementById("webview");
+
+urlInputField.addEventListener("keydown", (event) => {
+  if(event.key==="Enter"){
+    event.preventDefault();
+    handleUrl();
+  }
+})
+
+function handleUrl() {
+  let url = "";
+  const inputUrl = urlInputField.value;
+
+  if(inputUrl.startsWith("http://") || inputUrl.startsWith("https://")){
+    url = inputUrl;
+  } else {
+    url = "http://" + inputUrl
+  }
+  webview.src = url;
+
+}
